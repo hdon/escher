7e11c54c60c5e22735e43b1abe8964b9b1875fd4
Donny Viszneki <don@codebad.com>
2013-07-27 01:32:49 -0700

    blender tools: automatically creating escher portal material

--- blender/escher-tools.py
+++ blender/escher-tools.py
@@ -1,7 +1,5 @@
-#----------------------------------------------------------
-# File hello.py
-#----------------------------------------------------------
 import bpy, bmesh
+from mathutils import Color
 
 def selectedFaces(bm):
   return filter(lambda fa: fa.select, bm.faces)
@@ -13,9 +11,15 @@ def getEditMesh(cx):
         return bmesh.from_edit_mesh(ob.data)
   return None
 
-#
-#  Menu in window region, object context
-#
+def getPortalMaterial():
+  if 'EscherPortalMaterial' in bpy.data.materials:
+    return bpy.data.materials['EscherPortalMaterial']
+  mat = bpy.data.materials.new('EscherPortalMaterial')
+  mat.diffuse_color = Color((1,0,1))
+  mat.use_transparency = True
+  mat.alpha = 0.5
+  return mat
+
 class ObjectPanel(bpy.types.Panel):
   bl_label = "Escher (Object)"
   bl_space_type = "PROPERTIES"
@@ -23,22 +27,9 @@ class ObjectPanel(bpy.types.Panel):
   bl_context = "object"
  
   def draw(self, context):
-    print('eschertools.ObjectPanel.draw()')
     self.layout.operator("hello.hello", text='Bonjour').country = "France"
+    self.layout.operator("escher.portalize_face")
 
-    print('context: ', context)
-    print('bpy.context: ', bpy.context)
-    me = getEditMesh(context)
-    if me:
-      print('found active mesh')
-      fa = list(selectedFaces(me))
-      if len(fa):
-        print('found selected faces', list(map(lambda f:'%s select=%s' % (f, f.select), fa)))
-        self.layout.operator("hello.hello", text='Hello').country = "USA"
-
-#
-#  Menu in window region, material context
-#
 class MaterialPanel(bpy.types.Panel):
   bl_label = "Escher (Material)"
   bl_space_type = "PROPERTIES"
@@ -48,29 +39,31 @@ class MaterialPanel(bpy.types.Panel):
   def draw(self, context):
     self.layout.operator("hello.hello", text='Ciao').country = "Italy"
  
-#
-#  The Hello button prints a message in the console
-#
 class OBJECT_OT_HelloButton(bpy.types.Operator):
   bl_idname = "hello.hello"
   bl_label = "Say Hello"
   country = bpy.props.StringProperty()
 
   def execute(self, context):
-    print('OBJECT_OT_HelloButton() context: ', context)
-    for k in dir(context):
-      print('context.', k, ' = ', getattr(context, k))
-    if self.country == '':
-      print("Hello world!")
-    else:
-      print("Hello world from %s!" % self.country)
     return{'FINISHED'}  
  
-#
-#	Registration
-#   All panels and operators must be registered with Blender; otherwise
-#   they do not show up. The simplest way to register everything in the
-#   file is with a call to bpy.utils.register_module(__name__).
-#
- 
+class ESCHER_OT_Portalize_Face(bpy.types.Operator):
+  '''Portalize selected faces'''
+  bl_idname = "escher.portalize_face"
+  bl_label = "Portalize Mesh Face"
+  
+  def execute(self, context):
+    print(getPortalMaterial())
+    return {'FINISHED'}
+  
+  @classmethod
+  def poll(cls, cx):
+    me = getEditMesh(cx)
+    if me:
+      print('found active mesh')
+      fa = list(selectedFaces(me))
+      if len(fa):
+        return True
+    return False
+
 bpy.utils.register_module(__name__)
