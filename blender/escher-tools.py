b59b61dcd585eb42cc5b6c18293787e80ffd86d0
Donny Viszneki <don@codebad.com>
2013-07-30 19:05:53 -0700

    blender/escher-tools.py: "A lot of work on PSOs, SSOs, and SMs"

--- blender/escher-tools.py
+++ blender/escher-tools.py
@@ -20,16 +20,19 @@ def getPortalMaterial():
   mat.alpha = 0.5
   return mat
 
-class NPanel(bpy.types.Panel):
-  bl_label = 'Escher'
-  bl_space_type = 'VIEW_3D'
-  bl_region_type = 'TOOLS'
+class OBJECT_OT_NewSpaceButton(bpy.types.Operator):
+  bl_idname = 'escher.create_space'
+  bl_label = 'New Space'
   
-  def draw(self, context):
-    self.layout.operator('escher.portalize_face')
-    self.layout.operator('escher.link_remote')
-    self.layout.operator('escher.realize_remote')
-
+  def execute(self, cx):
+    # Create a new mesh representing the Esher space
+    # Create a new PSO (Primary Space Object) containing the mesh
+    # Insert PSO into scene
+    # If an EMPTY representing an Escher Remote is active
+    #   hide PSO
+    #   create SSO (Secondary Space Object) and parent to the EMPTY
+    return {'FINISHED'}
+    
 class OBJECT_OT_HelloButton(bpy.types.Operator):
   bl_idname = "hello.hello"
   bl_label = "Say Hello"
@@ -85,12 +88,13 @@ class ESCHER_OT_LinkRemote(bpy.types.Operator):
   def execute(self, cx):
     # This empty object represents the remote space
     eo = bpy.data.objects.new('EscherRemote', None)
-    bpy.context.scene.objects.link(eo)
+    cx.scene.objects.link(eo)
+    eo['escher_remote_space_name'] = '*none*'
     eo.parent = cx.object
     bpy.ops.object.select_all(action='DESELECT')
     eo.select = True
     bpy.ops.transform.translate()
-    return {'FINISHED'}
+    return bpy.ops.transform.translate('INVOKE_DEFAULT')
 
 class ESCHER_OT_RealizeRemote(bpy.types.Operator):
   '''Creates an object parented to the symbolic EMPTY object representing an Escher Remote.
@@ -121,4 +125,144 @@ class ESCHER_OT_RealizeRemote(bpy.types.Operator):
     remoteObj.parent = eo
     return {'FINISHED'}
 
-bpy.utils.register_module(__name__)
+class EscherSelectRemote(bpy.types.Operator):
+  """Select a remote space to be linked"""
+  bl_idname = "escher.select_remote"
+  bl_label = "Select Remote"
+  bl_options = {'REGISTER', 'UNDO'}
+  bl_property = "enumprop"
+
+  def item_cb(self, context):
+    return [('*new*', 'New Space', '')] + [(c.name, c.name[10:], '') for c in self.choices]
+
+  # This has to be a bpy.props.CollectionProperty(), it can't be a Python List!!!
+  choices = bpy.props.CollectionProperty(type=bpy.types.PropertyGroup)
+  enumprop = bpy.props.EnumProperty(items=item_cb)
+
+  @classmethod
+  def poll(cls, cx):
+    return cx.mode == 'OBJECT' and cx.object and cx.object.type == 'EMPTY' and \
+        'escher_remote_space_name' in cx.object
+
+  def execute(self, cx):
+    if self.enumprop == '*new*':
+      self.report({'INFO'}, 'TODO: Implement NEW')
+      return {'FINISHED'}
+    remotePsoName = self.enumprop
+    remoteSpaceName = psoName2spaceName(remotePsoName)
+    removeSsosFromRemoteEmpty(cx.object, cx)
+    cx.object['escher_remote_space_name'] = remoteSpaceName
+    sso = makeSSO(remoteSpaceName)
+    cx.scene.objects.link(sso)
+    sso.parent = cx.object
+    # TODO lock all transforms of the SSO
+    return {'FINISHED'}
+
+  def invoke(self, context, event):
+    self.choices.clear()
+    for ob in bpy.data.objects:
+      if ob.name.startswith('EscherPSO_'):
+        self.choices.add().name = ob.name
+    context.window_manager.invoke_search_popup(self)
+    return {'FINISHED'}
+
+class EscherNewSpace(bpy.types.Operator):
+  '''Create a new Escher Space, and focus on its PSO'''
+  bl_idname = 'escher.new_space'
+  bl_label = 'New Space'
+
+  newSpaceName = bpy.props.StringProperty(name='Escher Space Name', description='Name your new space', default='Unnamed_Space')
+
+  @classmethod
+  def poll(cls, cx):
+    return cx.mode == 'OBJECT'
+
+  def execute(self, cx):
+    pso = makePSO(self.newSpaceName)
+    cx.scene.objects.link(pso)
+    self.report({'INFO'}, 'Created new Space, mesh, and PSO')
+    return {'FINISHED'}
+    
+  def invoke(self, cx, ev):
+    return cx.window_manager.invoke_props_dialog(self) 
+
+def removeSsosFromRemoteEmpty(eo, cx):
+  for o in eo.children:
+    if isSsoName(o.name):
+      cx.scene.unlink(o)
+
+def makePSO(spaceName, me=None):
+  if me is None:
+    bm = bmesh.new()
+    for x in -1, 1:
+      for y in -1, 1:
+        for z in -1, 1:
+          bm.verts.new().co = (x, y, z)
+    bm.verts.index_update()
+    bm.faces.new((bm.verts[0], bm.verts[2], bm.verts[3], bm.verts[1]))
+    bm.faces.new((bm.verts[4], bm.verts[5], bm.verts[7], bm.verts[6]))
+    bm.faces.new((bm.verts[0], bm.verts[4], bm.verts[6], bm.verts[2]))
+    bm.faces.new((bm.verts[1], bm.verts[3], bm.verts[7], bm.verts[5]))
+    bm.faces.new((bm.verts[0], bm.verts[1], bm.verts[5], bm.verts[4]))
+    bm.faces.new((bm.verts[2], bm.verts[6], bm.verts[7], bm.verts[3]))
+    me = bpy.data.meshes.new(spaceName2meshName(spaceName))
+    bm.to_mesh(me)
+  pso = bpy.data.objects.new(spaceName2psoName(spaceName), me)
+  return pso
+
+def makeSSO(spaceName):
+  psoName = spaceName2psoName(spaceName)
+  if psoName not in bpy.data.objects:
+    raise KeyError('Could not find "%s"' % psoName)
+  ssoName = spaceName2ssoName(spaceName)
+  sso = bpy.data.objects.new(ssoName, bpy.data.objects[psoName].data)
+  return sso
+
+def isUnqualifiedSpaceName(spaceName):
+  return not (spaceName.startswith('EscherPSO_') or spaceName.startswith('EscherSSO_') or spaceName.startswith('EscherSM_'))
+
+def spaceName2meshName(spaceName):
+  if isUnqualifiedSpaceName(spaceName):
+    return 'EscherSM_' + spaceName
+  raise ValueError('Invalid space name!')
+
+def spaceName2psoName(spaceName):
+  if isUnqualifiedSpaceName(spaceName):
+    return 'EscherPSO_' + spaceName
+  raise ValueError('Invalid space name!')
+
+def spaceName2ssoName(spaceName):
+  if isUnqualifiedSpaceName(spaceName):
+    return 'EscherSSO_' + spaceName
+  raise ValueError('Invalid space name!')
+
+def psoName2spaceName(psoName):
+  if psoName.startswith('EscherPSO_'):
+    return psoName[10:]
+  else:
+    raise ValueError('Invalid Escher PSO name string')
+
+class NPanel(bpy.types.Panel):
+  bl_label = 'Escher Tools'
+  bl_space_type = 'VIEW_3D'
+  bl_region_type = 'TOOLS'
+  
+  def draw(self, cx):
+    layout = self.layout
+    layout.operator('escher.portalize_face')
+    layout.operator('escher.link_remote')
+    layout.operator('escher.realize_remote')
+    layout.operator('mesh.flip_normals', text='Flip Normals')
+    layout.operator('escher.select_remote')
+    layout.operator('escher.new_space')
+
+def register():
+  #bpy.types.Object.escher_space_name = bpy.props.EnumProperty(items=escher_space_names)
+  bpy.utils.register_module(__name__)
+
+def unregister():
+  #del bpy.types.Object.escher_space_name
+  bpy.utils.unregister_module(__name__)
+
+if __name__ == '__main__':
+  register()
