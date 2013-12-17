f2af3578d3e39718e5ac7cda4368c0ad749cefc6
Donny Viszneki <don@codebad.com>
2013-12-17 10:40:56 -0800

    added initialization script

--- src/ants/main.d
+++ src/ants/main.d
@@ -1,16 +1,46 @@
 import display;
 import std.stdio;
+import std.string : splitLines, split;
+import file = std.file;
 
 int main(string[] args)
 {
-  if (args.length != 2)
+  string mapfilename;
+  if (args.length == 2)
   {
-    writefln("usage: %s <map.esc>", args[0]);
-    return 1;
+    mapfilename = args[1];
+  }
+  else
+  {
+    foreach (lineNo, line; splitLines(to!string(cast(char[])file.read("init.txt"))))
+    {
+      if (lineNo == 0)
+      {
+        if (line != "escher engine init script version 1")
+        {
+          writeln("Could not find Escher engine initialization script!");
+          return 1;
+        }
+        continue;
+      }
+
+      auto words = split(line);
+      if (words.length == 0)
+        continue;
+      switch (words[0])
+      {
+        case "map":
+          mapfilename = words[1];
+          break;
+        default:
+          writefln("error: init.txt: unknown command \"%s\"", words[0]);
+          return 1;
+      }
+    }
   }
 
   bool isRunning = true;
-  Display display = new Display(args[1]);
+  Display display = new Display(mapfilename);
   scope(exit) display.cleanup();
 
   while (isRunning)
