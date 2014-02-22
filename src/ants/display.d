2888cc490e8ba39557ceb6652ece9bbc1e95215e
rtmf <livinglatexkali@gmail.com>
2014-02-22 00:47:01 -0500

    add version(stencil) check for requesting stencil visual

--- src/ants/display.d
+++ src/ants/display.d
@@ -89,7 +89,7 @@ class Display
     SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 8);
     SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 8);
     SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE, 8);
-    SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
+    version(stencil) SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
     //SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
 
 //	glErrorCheck();
